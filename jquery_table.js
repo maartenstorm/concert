322ce6063a22b26616aaee22d90ea4ddc8bd903c
Maarten Storm <storm.maarten@gmail.com>
2012-05-05 18:33:21 +0200

    Fixed date display standard style. Table also expanding on info table. Removed node link from info table.

--- jquery_table.js
+++ jquery_table.js
@@ -1,8 +1,12 @@
 (function ($) {
   $(document).ready(function() {
 
-   $('td .item-list').click(function () { 
+   $('td.expand').click(function () { 
       //$('tr.child', $(this).parent()).slideToggle("fast");
+      $(this).parents('tr.parent').nextUntil('tr.parent').slideToggle("fast");
+   });
+   
+   $('td .expand').click(function () {
       $(this).parents('tr.parent').nextUntil('tr.parent').slideToggle("fast");
    });
 
